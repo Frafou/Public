db23bd3c717db19efc6cad2579a9748fb165aca4
Francois Fournier <v-ffournier@microsoft.com>
2025-09-02 19:59:30 -0400

    1959

---
+++ 2021-05-21 Azure Functions for PowerShell/presentation.ps1
@@ -0,0 +1,47 @@
+﻿# Failsafe
+return
+
+<#
+Questions?
+- Raise Hand
+- Unmute and Ask
+- Write in Chat
+#>
+
+<#
+Links:
+Recordings: aka.ms/pssession
+Materials: aka.ms/pssession-docs
+Chat: aka.ms/mspowershell
+#>
+
+# Why Azure Functions (And why PowerShell)
+<#
+- Task Scheduler on Azure
+- Secure Delegation
+- Integration Automation Workflows
+
+- Already know it
+#>
+
+# Setting up a Function App
+
+
+# Granting Permissions on Resources
+
+
+# Basic Layout
+Set-Location 'C:\Session\PowerShell Azure Functions\DemoFunction'
+# PSModuleDevelopment
+# Install-Module PSModuleDevelopment
+Invoke-PSMDTemplate AzureFunction -Name DemoFunction -Path . -NoFolder
+Invoke-PSMDTemplate AzureFunctionRest -Name GetVM
+
+<#
+- Get/List VM
+- Start VMs
+- Stop VMs
+#>
+
+# The Pipeline
+# Modules
